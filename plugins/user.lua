8f48df49cd54a372c1f34a9c5ad8623dfce3bdab
jin2461 <joshischwartz1@gmail.com>
2023-04-04 07:45:20 -0400

    add nvim dap, also reformated for cleaner code

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,10 +1,11 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  {
-    "kdheepak/lazygit.nvim",
-    lazy = false,
-  }
+  -- plugins = {
+
+  { "kdheepak/lazygit.nvim", lazy = false },
+  { "mfussenegger/nvim-dap", enabled = true },
+  -- }
   -- "andweeb/presence.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",

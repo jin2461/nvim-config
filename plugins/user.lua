25a3f57c59fdeb9a7259b758887329b20bbd5879
jin2461 <joshischwartz1@gmail.com>
2023-04-04 07:05:19 -0400

    added lazygit

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,6 +1,10 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = false,
+  }
   -- "andweeb/presence.nvim",
   -- {
   --   "ray-x/lsp_signature.nvim",

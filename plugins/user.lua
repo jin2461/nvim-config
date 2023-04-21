c11fab9e5580e4c56ad2c9a49b782d230b2f663c
jin2461 <joshischwartz1@gmail.com>
2023-04-21 06:33:06 -0400

    added pico 8

--- plugins/user.lua
+++ plugins/user.lua
@@ -3,4 +3,10 @@ return {
   { "kdheepak/lazygit.nvim", lazy = false },
   { "mfussenegger/nvim-dap", enabled = true },
   { "folke/tokyonight.nvim" },
+  {
+    "bakudankun/pico-8.vim",
+    ft = "pico8",
+    pico8_pah = "C:/Users/admin/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/PICO-8"
+  },
+  -- { "justinj/vim-pico8-syntax", lazy = false },
 }
